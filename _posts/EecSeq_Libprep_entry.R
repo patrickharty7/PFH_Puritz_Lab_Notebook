1ad70668a077a22e1bd6a43aea9d8cb51c73ee65
Finn Harty <84587242+patrickharty7@users.noreply.github.com>
2021-07-21 12:58:31 -0400

    Update EecSeq_Libprep_entry.R

--- _posts/EecSeq_Libprep_entry.R
+++ _posts/EecSeq_Libprep_entry.R
@@ -1,5 +1,6 @@
 7/1/2021
 # EecSeq Library Prep
 ### Goals:
-* Determine concentration per sample in pooled library
+* Determine concentration per sample needed for 4000 ng between 12 samples
+* Using concentration per ul determine volume per sample needed
 * 
