c22df86c2a243ded42ae3793343faea297565157
Finn Harty <84587242+patrickharty7@users.noreply.github.com>
2021-07-21 13:14:22 -0400

    Update EecSeq_Libprep_entry.R

--- _posts/EecSeq_Libprep_entry.R
+++ _posts/EecSeq_Libprep_entry.R
@@ -3,4 +3,21 @@
 ### Goals:
 * Determine concentration per sample needed for 4000 ng between 12 samples
 * Using concentration per ul determine volume per sample needed
-* 
+* Create 3 pools for each Library, 150 bp fragment size, 250 bp fragment size, 350 bp fragment size
+* Use KAPA mRNA Hyperprep kit to prepare libraries
+* Quantify data using tapestation and Qubit
+* Proceed to next step in EecSeq
+
+### Volume Per sample needed for 4000 ng, 333.33 ng/sample
+* nar.7.3.g = 0.74 ul
+* nar.8.3.g = 2.28 ul
+* nar.9.3.g = 2.25 ul
+* ghp.1.3.g = 1.94 ul
+* ghp.2.3.g = 0.93 ul
+* ghp.4.3.g = 0.94 ul
+* bar.1.3.g = 2.01 ul
+* bar.2.3.g = 1.57 ul
+* kic.1.3.g = 1.07 ul
+* kic.2.3.g = 2.84 ul
+* mcd.1.3.g = 1.35 ul
+* mcd.2.3.g = 1.52 ul
